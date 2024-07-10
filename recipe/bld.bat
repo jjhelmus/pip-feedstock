f74cc0e2c4cbfac4123196a3cf4a36b21fe36e8e
Jonathan Helmus <jjhelmus@gmail.com>
2024-07-10 12:12:01 -0500

    use whl to install package, remove dep on setuptools and wheel

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,5 +1,5 @@
-set PYTHONPATH=".\src"
-%PYTHON% -m pip install . --no-deps --no-build-isolation -vv
+set PYTHONPATH=".\pip-%PKG_VERSION%-py3-none-any.whl"
+%PYTHON% -m pip install --no-cache-dir --no-index --no-index --find-links . pip
 if errorlevel 1 exit 1
 
 cd %SCRIPTS%
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# use the pip source to install itself
-PYTHONPATH="./src" $PYTHON -m pip install . --no-deps --no-build-isolation -vv
+# use the pip wheel to install itself
+PYTHONPATH='./pip-${PKG_VERSION}-py3-none-any.whl' $PYTHON -m pip install --no-cache-dir --no-index --no-index --find-links . pip
 
 cd $PREFIX/bin
 rm -f pip2* pip3*
